4121bb26b1ee767ace19b7f56d2bcf9fb0ec41b0
Hao-Xin Lin <43382049+HxLyn3@users.noreply.github.com>
2025-01-26 21:06:22 +0800

    update env

--- env/__init__.py
+++ env/__init__.py
@@ -6,5 +6,7 @@ from .mujoco_env import make_mujoco_env
 ENV = {
     "mujoco": make_mujoco_env,
     "d4rl": lambda env_name: gym.make(env_name),
-    "neorl": lambda env_name: neorl.make(env_name)
+    "neorl": lambda env_name: neorl.make(env_name),
+    "adroit": lambda env_name: gym.make(env_name),
+    "maze": lambda env_name: gym.make(env_name)
 }
